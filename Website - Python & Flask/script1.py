578f4094decd82985e8938cb581c98e669940bdb
vikrantRajan <vikrantrajan93@gmail.com>
2020-10-21 21:07:59 -0700

    Setup Python Flask App + Three.js

--- Website - Python & Flask/script1.py
+++ Website - Python & Flask/script1.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/static")
 
 @app.route('/')
 def home():
-    return render_template("index2.html")
+    return render_template("index5.html")
 
 @app.route('/about/')
 def about():
-    return "<h1>About Page content goes here</h1>"
+    return render_template("index2.html")
 
 if __name__ =="__main__":
     app.run(debug=True)
